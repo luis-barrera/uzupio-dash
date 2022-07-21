26f02528975e59bee4149dcceee0d13740f31cba
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-07-21 09:34:13 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv("uzupio.csv")
+# df = df.iloc[::-1]
+
+fig = px.bar(df,
+             x = "Dictamen",
+             y = ["Adhieren", "No se Adhieren", "Abstienen"],
+             # text = "Dictamen",
+             height = 1000,
+             title = "Votación de la Constitución de la República de Uzupio")
+
+fig.update_layout(legend=dict(
+    yanchor="top",
+    y=1.08,
+    xanchor="right",
+    x=0.5
+))
+# fig.update_xaxes(title_text = "Votantes")
+# fig.update_yaxes(showtick=False)
+
+config = {"responsive": True}
+
+app.layout = html.Div(children=[
+    html.H1(children='Uzupio'),
+
+    html.Div(children='''
+        Por luis-barrera
+    '''),
+    dcc.Graph(id='example-graph',
+                      figure=fig,
+                      config=config)
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
