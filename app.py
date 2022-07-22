30d36adc362c0408e206b3aee6a14e647ed4c513
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-07-21 23:53:44 -0500

    Barras verticales

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ server = app.server
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_csv("https://storage.googleapis.com/small-projects-luis-barrera/uzupio.csv")
-# df = df.iloc[::-1]
+df = df.iloc[::-1]
 
 fig = px.bar(df,
-             x = "Dictamen",
-             y = ["Adhieren", "No se Adhieren", "Abstienen"],
+             y = "Dictamen",
+             x = ["Adhieren", "No se Adhieren", "Abstienen"],
              # text = "Dictamen",
-             height = 1000,
+             height = 2000,
              title = "Votación de la Constitución de la República de Uzupio")
 
 fig.update_layout(legend=dict(
@@ -32,8 +32,9 @@ fig.update_layout(legend=dict(
     xanchor="right",
     x=0.5
 ))
-# fig.update_xaxes(title_text = "Votantes")
-# fig.update_yaxes(showtick=False)
+
+fig.update_xaxes(title_text = "Votantes")
+fig.update_yaxes(showticklabels = False)
 
 config = {"responsive": True}
 
